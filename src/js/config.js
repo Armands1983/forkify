ac4949fb44bb801cb4fc048fab8b934deed9d73c
Armands1983 <trencis.armands@gmail.com>
2023-05-06 19:45:23 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = '45c3cca8-302f-4b33-a0b8-797eec13cd18';
+
+export const MODAL_CLOSE_SEC = 2.5;
